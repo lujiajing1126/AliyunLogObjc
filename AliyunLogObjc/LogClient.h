02cb1223b8e30751cefacaed7150c87db50ee9c2
megrez <lujiajing1126@gmail.com>
2016-11-23 20:07:29 +0800

    Fix Params & Add _Nullable

--- AliyunLogObjc/LogClient.h
+++ AliyunLogObjc/LogClient.h
@@ -27,7 +27,7 @@
 - (NSString*)GetEndPoint;
 - (NSString*)GetKeyID;
 - (NSString*)GetKeySecret;
-- (void)PostLog:(LogGroup*)logGroup logStoreName:(NSString*)name call:(void (^)(NSURLResponse*,NSError*) )errorCallback;
+- (void)PostLog:(LogGroup*)logGroup logStoreName:(NSString*)name call:(void (^)(NSURLResponse* _Nullable response,NSError* _Nullable error) )errorCallback;
 
 @end
 
